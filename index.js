b813269b41df60f28f45caf9871f61603c52ee7e
varmanli <varmanliamirhosein@gmail.com>
2024-12-08 03:45:00 +0330

    fix commit

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const connectDB = require("./config/db");
 const app = express();
 const artistRoutes = require("./routes/artistsRoute");
 const musicRoutes = require("./routes/musicRoute");
+const cors = require("cors");
 
 connectDB();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173/", // برای تست، همه منابع مجاز هستند. در Production، آدرس دقیق فرانت‌اند را وارد کنید.
+    methods: ["GET", "POST", "PUT", "DELETE"], // متدهای مجاز
+    credentials: true, // در صورت نیاز به ارسال کوکی‌ها
+  })
+);
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/music", musicRoutes);
 app.use("/api/artists", artistRoutes);
